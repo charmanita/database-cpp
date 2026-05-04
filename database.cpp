798e1a117d4511c6d6d47fdb69ab03a0877068f0
charmanita <hdroberson23@gmail.com>
2026-05-03 23:47:53 -0500

    Added some comments in the code.

--- database.cpp
+++ database.cpp
@@ -1,67 +1,80 @@
-#include <iostream> 
-#include <fstream> 
-#include <string> 
-#include <ctime> 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
 
-using namespace std; 
+using namespace std;
+// This program is a basic implementation of a database in C++ with the use of ifstream and ofstream.
+int main()
+{
+    string name, extraInfo, line;
+    char action, choice;
 
-int main() {
-    string name, extraInfo, line; 
-    char action, choice; 
-
-    do {
+    do
+    {
         cout << "\nWould you like to (W)rite a new entry or (R)ead a File? ";
-        cin >> action; 
-        cout << "Name: "; 
-        cin >> name; 
-
+        cin >> action;
+        cout << "Name: ";
+        cin >> name;
+        // The database files are saved in txt.
         string fileName = name + ".txt";
 
-        if (action == 'r' || action == 'R') {
+        if (action == 'r' || action == 'R')
+        {
             ifstream inFile(fileName);
-            if (inFile.is_open()) {
-                cout << "\n--- Viewing File: " << fileName << " ---\n"; 
-                while (getline(inFile, line)) {
+            if (inFile.is_open())
+            {
+                cout << "\n--- Viewing File: " << fileName << " ---\n";
+                while (getline(inFile, line))
+                {
                     cout << line << endl;
                 }
                 inFile.close();
-            } else {
-                cout << "No record found for " << name << endl; 
             }
-        } 
-        else {
+            else
+            {
+                cout << "No record found for " << name << endl;
+            }
+        }
+        else
+        {
             ifstream checkFile(fileName);
             bool isNewFile = !checkFile.is_open();
             checkFile.close();
 
             ofstream outFile(fileName, ios::app);
+            // if file is open, then time is grabbed and gets added into a timestamp.
+            // With new data being appended at EOF.
+            if (outFile.is_open())
+            {
+                time_t now = time(0);
+                string dt = ctime(&now);
+                dt.pop_back();
 
-            if (outFile.is_open()) {
-                time_t now = time(0); 
-                string dt = ctime(&now); 
-                dt.pop_back(); 
-
-                if (isNewFile) {
-                    outFile << "Name: " << name << endl; 
-                    outFile << "-----------------" << endl; 
+                if (isNewFile)
+                {
+                    outFile << "Name: " << name << endl;
+                    outFile << "-----------------" << endl;
                 }
-                
+
                 cout << "What would you like to add to " << name << "'s file? ";
                 getline(cin >> ws, extraInfo);
 
                 outFile << "[" << dt << "]" << endl;
-                outFile << extraInfo << endl; 
-                outFile << "-----------------" << endl; 
-                outFile.close(); 
-                cout << "Info saved with timestamp!" << endl; 
-            } else {
-                cerr << "Error opening file!" << endl; 
+                outFile << extraInfo << endl;
+                outFile << "-----------------" << endl;
+                outFile.close();
+                cout << "Info saved with timestamp!" << endl;
+            }
+            else
+            {
+                cerr << "Error opening file!" << endl;
             }
         }
 
         // --- Crucial Addition: Ask to continue ---
         cout << "\nWould you like to perform another action? (y/n): ";
-        cin >> choice; 
+        cin >> choice;
 
     } while (choice == 'y' || choice == 'Y');
 
